a61c8bb603fc2cba32b8757e60fa5f91bc55d11c
Vincent Nguyen <vpvnguyen@gmail.com>
2020-03-14 16:41:36 -0700

    Vue socks product page containing: binding attributes, conditional rendering, list rendering, and event handling

---
+++ intro-to-vue/main.js
@@ -0,0 +1,40 @@
+var product = "socks";
+
+// creates a new Vue instance
+var app = new Vue({
+  // plug into id=app
+  el: "#app",
+  data: {
+    product: "socks",
+    image: "./vmSocks-green-onWhite.jpg",
+    inStock: true,
+    inventory: 10,
+    details: ["80% cotton", "20% polyester", "Gender-neutral"],
+    variants: [
+      {
+        variantId: 2234,
+        variantColor: "green",
+        variantImage: "./vmSocks-green-onWhite.jpg"
+      },
+      {
+        variantId: 2235,
+        variantColor: "blue",
+        variantImage: "./vmSocks-blue-onWhite.jpg"
+      }
+    ],
+    cart: 0,
+    github: "https://github.com/vpvnguyen/vuetify-reference"
+  },
+  methods: {
+    addToCart: function() {
+      this.cart += 1;
+    },
+    removeFromCart: function() {
+      this.cart -= 1;
+    },
+    // ES6 shorthand
+    updateProduct(variantImage) {
+      this.image = variantImage;
+    }
+  }
+});
